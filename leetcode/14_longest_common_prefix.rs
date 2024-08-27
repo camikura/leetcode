e89c3c71169ed3b6c55bd78ee8efff80d2abebd0
Akira Kamikura <akira.kamikura@gmail.com>
2024-08-27 15:35:01 +0900

    rust version for longest common prefix

---
+++ leetcode/14_longest_common_prefix.rs
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=14 lang=rust
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+impl Solution {
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        let mut prefix = strs[0].clone();
+        for (i, s) in strs[1..].iter().enumerate() {
+            if i != 0 {
+                while !s.starts_with(&prefix) {
+                    prefix.pop();
+                    if prefix.is_empty() {
+                        return String::new();
+                    }
+                }
+            }
+        }
+
+        prefix
+    }
+}
+// @lc code=end
