f41d809059a77cb2671bc66adc409dd6f0d37b46
camikura <akira.kamikura@gmail.com>
2024-09-02 14:33:28 +0900

    add [28] Find the Index of the First Occurrence in a String

---
+++ src/28_find_the_index_of_the_first_occurrence_in_a_string.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=28 lang=cpp
+ *
+ * [28] Find the Index of the First Occurrence in a String
+ */
+
+#ifndef LEETCODE_H
+#define LEETCODE_H
+
+#include <string>
+using namespace std;
+
+#endif
+
+// @lc code=start
+class Solution {
+ public:
+  int strStr(string haystack, string needle) { return haystack.find(needle); }
+};
+// @lc code=end
