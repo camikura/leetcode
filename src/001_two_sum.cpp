c2dd046969d80931fc01056a6f67c9cabc887f94
Akira Kamikura <akira.kamikura@gmail.com>
2024-09-03 11:45:24 +0900

    use gtest for unit test (#19)

---
+++ src/001_two_sum.cpp
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+#include <vector>
+#include <map>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+// @lc code=start
+class Solution {
+ public:
+  std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    std::map<int, int> m;
+
+    for (auto i = 0; i < nums.size(); i++) {
+      int num = nums[i];
+      int diff = target - num;
+      auto it = m.find(diff);
+      if (it != m.end()) {
+        return {it->second, i};
+      }
+      m[num] = i;
+    }
+
+    return {};
+  }
+};
+// @lc code=end
+
+TEST(two_sum, case1) {
+  vector<int> nums{2, 7, 11, 15};
+  vector<int> result{0, 1};
+  int target = 9;
+  EXPECT_EQ(result, Solution().twoSum(nums, target));
+}
+
+TEST(two_sum, case2) {
+  vector<int> nums{3, 2, 4};
+  vector<int> result{1, 2};
+  int target = 6;
+  EXPECT_EQ(result, Solution().twoSum(nums, target));
+}
+
+TEST(two_sum, case3) {
+  vector<int> nums{3, 3};
+  vector<int> result{0, 1};
+  int target = 6;
+  EXPECT_EQ(result, Solution().twoSum(nums, target));
+}
