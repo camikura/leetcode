c2d565ff10c5fd95ff121eba379b274a342ad62d
Akira Kamikura <akira.kamikura@gmail.com>
2024-09-06 12:04:33 +0900

    add [3] Longest Substring Without Repeating Characters

--- include/leetcode.h
+++ include/leetcode.h
@@ -1,29 +1,30 @@
+#include <gtest/gtest.h>
 #include <iostream>
 #include <map>
 #include <queue>
 #include <stack>
 #include <string>
+#include <unordered_set>
 #include <vector>
-#include <gtest/gtest.h>
 
 using namespace std;
 
 /** Definition for singly-linked list. */
 struct ListNode {
   int val;
-  ListNode *next;
+  ListNode* next;
   ListNode() : val(0), next(nullptr) {}
   ListNode(int x) : val(x), next(nullptr) {}
-  ListNode(int x, ListNode *next) : val(x), next(next) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
 /** Definition for a binary tree node. */
 struct TreeNode {
   int val;
-  TreeNode *left;
-  TreeNode *right;
+  TreeNode* left;
+  TreeNode* right;
   TreeNode() : val(0), left(nullptr), right(nullptr) {}
   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right)
+  TreeNode(int x, TreeNode* left, TreeNode* right)
       : val(x), left(left), right(right) {}
 };
---
+++ src/003_longest_substring_without_repeating_characters.cc
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=3 lang=cpp
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+#ifdef NOT_LEETCODE
+#include <leetcode.h>
+#endif
+
+// @lc code=start
+class Solution {
+ public:
+  int lengthOfLongestSubstring(string s) {
+    unordered_set<char> seen;
+    int start = 0, maxLength = 0;
+
+    for (int end = 0; end < s.size(); end++) {
+      while (seen.find(s[end]) != seen.end()) {
+        seen.erase(s[start]);
+        start++;
+      }
+      seen.insert(s[end]);
+      maxLength = max(maxLength, end - start + 1);
+    }
+
+    return maxLength;
+  }
+};
+// @lc code=end
+
+#ifdef NOT_LEETCODE
+TEST(p003_longest_substring_without_repeating_characters, case1) {
+  EXPECT_EQ(3, Solution().lengthOfLongestSubstring("abcabcbb"));
+}
+
+TEST(p003_longest_substring_without_repeating_characters, case2) {
+  EXPECT_EQ(1, Solution().lengthOfLongestSubstring("bbbbb"));
+}
+
+TEST(p003_longest_substring_without_repeating_characters, case3) {
+  EXPECT_EQ(3, Solution().lengthOfLongestSubstring("pwwkew"));
+}
+#endif
